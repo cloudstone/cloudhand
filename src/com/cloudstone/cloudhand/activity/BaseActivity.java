e888a76062a1c1b46659a19367192647613267fc
xuhf <xuhf@fenbi.com>
2013-07-06 14:18:13 +0800

    ADD : Cloudhand Application

---
+++ src/com/cloudstone/cloudhand/activity/BaseActivity.java
@@ -0,0 +1,15 @@
+/**
+ * @(#)BaseActivity.java, 2013-7-6. 
+ * 
+ */
+package com.cloudstone.cloudhand.activity;
+
+import android.app.Activity;
+
+/**
+ * @author xuhongfeng
+ *
+ */
+public class BaseActivity extends Activity  {
+
+}
