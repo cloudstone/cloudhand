4e7e1edcf43bebc926f148d7d9cd5190a4fe4b37
xuhf <xuhf@fenbi.com>
2013-08-11 14:08:01 +0800

    ADD: Dish datastructure

---
+++ src/com/cloudstone/cloudhand/constant/Const.java
@@ -0,0 +1,18 @@
+/**
+ * @(#)Const.java, Aug 11, 2013. 
+ * 
+ */
+package com.cloudstone.cloudhand.constant;
+
+/**
+ * @author xuhongfeng
+ *
+ */
+public class Const {
+    
+    public static class DishStatus {
+        public static final int STATUS_INIT = 0;
+        public static final int STATUS_IN_MENU = 1;
+    }
+
+}
