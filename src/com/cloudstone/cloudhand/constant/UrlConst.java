554eb36b88c504830cc3f4d28390dada5de3ec7c
xhc <life.xhc@gmail.com>
2013-08-26 11:56:37 -0700

    ADD:获取桌况

--- src/com/cloudstone/cloudhand/constant/UrlConst.java
+++ src/com/cloudstone/cloudhand/constant/UrlConst.java
@@ -10,7 +10,7 @@ package com.cloudstone.cloudhand.constant;
  */
 public class UrlConst {
     
-    public static final String DEFAULT_IP = "192.168.0.104";
+    public static final String DEFAULT_IP = "192.168.0.101";
     public static final int PORT = 8080;
 
     public static final String BASE_URL = "/api";
@@ -18,4 +18,8 @@ public class UrlConst {
     public static final String LIST_USER_NAME_URL = BASE_URL + "/public/user-names";
     public static final String LOGIN_URL = BASE_URL + "/login";
     public static final String LIST_DISH_URL = BASE_URL + "/dishes";
+    
+    public final String BASE_TABLE_URL = BASE_URL + "/tables";
+    public final String LIST_TABLE_URL = BASE_TABLE_URL;
+    private final String OCCUPY_TABLE_URL =  BASE_TABLE_URL + "/%d/occupy";
 }
--- src/com/cloudstone/cloudhand/dialog/TableInfoDialogFragment.java
+++ src/com/cloudstone/cloudhand/dialog/TableInfoDialogFragment.java
@@ -1,11 +1,24 @@
 package com.cloudstone.cloudhand.dialog;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
 import com.cloudstone.cloudhand.R;
+import com.cloudstone.cloudhand.data.Table;
+import com.cloudstone.cloudhand.exception.ApiException;
+import com.cloudstone.cloudhand.network.api.ListTableApi;
+import com.cloudstone.cloudhand.network.api.base.IApiCallback;
+import com.cloudstone.cloudhand.util.L;
 
 /**
  * 
@@ -13,6 +26,10 @@ import com.cloudstone.cloudhand.R;
  *
  */
 public class TableInfoDialogFragment extends BaseAlertDialogFragment {
+	ListView lvTableInfo;
+    SimpleAdapter adapter;
+    
+    private List<Map<String, String>> data = new ArrayList<Map<String,String>>();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -24,7 +41,52 @@ public class TableInfoDialogFragment extends BaseAlertDialogFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.dialog_table_info, container, false);
+        lvTableInfo = (ListView)view.findViewById(R.id.listview_table_info);
         return view;
+    }
+    
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        
+      //获取桌况
+        new ListTableApi().asyncCall(new IApiCallback<List<Table>>() {
+
+            @Override
+            public void onSuccess(List<Table> result) {
+                for(int i = 0;i < result.size();i++) {
+                    Map<String, String> map = new HashMap<String, String>();
+                    map.put("tableName", result.get(i).getName());
+                    if(result.get(i).getStatus() == 0) {
+                        map.put("tableStatus", getString(R.string.empty));
+                    } else if (result.get(i).getStatus() == 1) {
+                        map.put("tableStatus", getString(R.string.occupied));
+                    } else {
+                        map.put("tableStatus", getString(R.string.ordered));
+                    }
+                    data.add(map);
+                }
+                render();
+            }
+
+            @Override
+            public void onFailed(ApiException exception) {
+                Toast.makeText(getActivity(), R.string.error_list_table_info_failed, Toast.LENGTH_SHORT).show();
+                L.e(TableInfoDialogFragment.class, exception);
+            }
+
+            @Override
+            public void onFinish() {
+            }
+
+        });
+    }
+    
+    private void render() {
+        SimpleAdapter adapter = new SimpleAdapter(getActivity(), data,R.layout.view_table_info_dropdown_list_line, 
+                new String[] {"tableName", "tableStatus"}, 
+                new int[]{R.id.tv_table_name, R.id.tv_table_status});
+        lvTableInfo.setAdapter(adapter);
     }
     
 }
