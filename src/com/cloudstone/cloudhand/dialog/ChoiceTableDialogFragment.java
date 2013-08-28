7cc576638120e14f55f7466f5d2cd2be06dcfad0
xhc <life.xhc@gmail.com>
2013-08-28 01:06:27 -0700

    ADD:选桌子和开台

--- src/com/cloudstone/cloudhand/dialog/ChoiceTableDialogFragment.java
+++ src/com/cloudstone/cloudhand/dialog/ChoiceTableDialogFragment.java
@@ -52,6 +52,18 @@ public class ChoiceTableDialogFragment extends BaseAlertDialogFragment {
         tvCustomerNumber = (EditText)view.findViewById(R.id.edit_password);
         btnConfirm = (Button)view.findViewById(R.id.button_confirm);
         btnCancle = (Button)view.findViewById(R.id.button_cancle);
+        
+        //输入桌名文本框被点时自动获取桌子列表
+        tvTableName.setOnClickListener(new OnClickListener() {
+            
+            @Override
+            public void onClick(View v) {
+                if(tvTableName.getText().toString().equals("")) {
+                    tvTableName.showDropDown();
+                }
+            }
+        });
+        
         return view;
     }
     
@@ -59,7 +71,7 @@ public class ChoiceTableDialogFragment extends BaseAlertDialogFragment {
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         
-      //获取桌况
+        //获取桌况
         new ListTableApi().asyncCall(new IApiCallback<List<Table>>() {
 
             @Override
@@ -67,14 +79,12 @@ public class ChoiceTableDialogFragment extends BaseAlertDialogFragment {
                 tableNames = new String[result.size()];
                 for(int i = 0;i < result.size();i++) {
                     tableNames[i] = result.get(i).getName();
-                    System.out.println(tableNames[i]);
                 }
                 render();
             }
 
             @Override
             public void onFailed(ApiException exception) {
-                System.out.println("onFailed");
                 Toast.makeText(getActivity(), R.string.error_list_table_info_failed, 0).show();
                 L.e(ChoiceTableDialogFragment.class, exception);
             }
@@ -86,65 +96,62 @@ public class ChoiceTableDialogFragment extends BaseAlertDialogFragment {
 
         });
         
-        
+        //确定
         btnConfirm.setOnClickListener(new OnClickListener() {
             
             @Override
             public void onClick(View v) {
                 int tableId = 0;
+                //获取输入的桌名的ID
                 for(int i = 0;i < tableNames.length;i++) {
                     if(tvTableName.getText().toString().equals(tableNames[i])) {
                         tableId = i + 1;
                         break;
                     }
                 }
-                System.out.println("tableId -->" + tableId);
+                //判断输入的桌子是否存在
                 if(tableId > 0) {
+                    //判断输入的顾客人数是否为非0正整数
                     if(isInt(tvCustomerNumber.getText().toString())) {
                         int customerNumber = Integer.parseInt(tvCustomerNumber.getText().toString());
-                        if(customerNumber != 0){
-                            new OccupyTableApi(tableId,customerNumber).asyncCall(new OccupyTableCalback() {
-                                
-                                @Override
-                                public void onSuccess(Table result) {
-                                    System.out.println("onSuccess");
-                                }
-                                
-                                @Override
-                                public void onFinish() {
-                                    
-                                }
-                                
-                                @Override
-                                protected void onOccupied() {
-                                    
-                                }
-                                
-                                @Override
-                                protected void onError(ApiException e) {
-                                    
-                                }
-                            });
+                        new OccupyTableApi(tableId,customerNumber).asyncCall(new OccupyTableCalback() {
                             
-                            Intent intent = new Intent();
-                            intent.setClass(getActivity(), OpenTableActivity.class);
-                            startActivity(intent);
-                            dismiss();
-                        } else {
-                            Toast.makeText(getActivity(), R.string.choice_table_error3, 0).show();
-                        }
+                            @Override
+                            public void onSuccess(Table result) {
+                                Intent intent = new Intent();
+                                intent.setClass(getActivity(), OpenTableActivity.class);
+                                startActivity(intent);
+                                dismiss();
+                            }
+                            
+                            @Override
+                            public void onFinish() {
+                                
+                            }
+                            
+                            @Override
+                            protected void onOccupied() {
+                                
+                            }
+                            
+                            @Override
+                            protected void onError(ApiException e) {
+                                
+                            }
+                        });
                         
                     } else {
-                        Toast.makeText(getActivity(), R.string.choice_table_error2, 0).show();
+                        Toast.makeText(getActivity(), R.string.choice_table_customer_number_error, Toast.LENGTH_SHORT).show();
                     }
                     
                 } else {
-                    Toast.makeText(getActivity(), R.string.choice_table_error1, 0).show();
+                    Toast.makeText(getActivity(), R.string.choice_table_table_nonentity_error, Toast.LENGTH_SHORT).show();
                 }
                 
             }
         });
         
+        //取消
         btnCancle.setOnClickListener(new OnClickListener() {
             
             @Override
@@ -155,10 +162,14 @@ public class ChoiceTableDialogFragment extends BaseAlertDialogFragment {
         });
     }
     
+    //判断一个字符串是否为非0正整数
     private boolean isInt(String str) { 
         try { 
-            Integer.parseInt(str) ; 
-            return true ; 
+            if(Integer.parseInt(str) > 0) {
+                return true; 
+            } else {
+                return false;
+            }
         } catch (NumberFormatException e) { 
             return false;
         }
@@ -167,7 +178,7 @@ public class ChoiceTableDialogFragment extends BaseAlertDialogFragment {
     private void render() {
         //创建一个下拉框适配器
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
-                R.layout.view_base_dropdown_list, tableNames); 
+                R.layout.view_base_dropdown_list_line, tableNames); 
         //设置下拉列表风格
         adapter.setDropDownViewResource(R.layout.view_base_dropdown_list_line);
         
