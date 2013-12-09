c8ddf8d690754a4ee430b4ba792115c52cf3d0a8
xhc <life.xhc@gmail.com>
2013-12-09 11:26:05 +0800

    ADD:在登录添加数据库功能

--- src/com/cloudstone/cloudhand/db/DBOpenHelper.java
+++ src/com/cloudstone/cloudhand/db/DBOpenHelper.java
@@ -2,7 +2,6 @@ package com.cloudstone.cloudhand.db;
 
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteDatabase.CursorFactory;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class DBOpenHelper extends SQLiteOpenHelper {
@@ -16,9 +15,8 @@ public class DBOpenHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String sql = "create table if not exists user(id integer primary key autoincrement, name varchar(64), password varchar(64))";
+        String sql = "create table if not exists user(id integer primary key autoincrement, name varchar(64))";
         db.execSQL(sql);
-
     }
 
     @Override
--- src/com/cloudstone/cloudhand/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.cloudstone.cloudhand.service;
-
-public interface UserService {
-    
-    public void addUser(Object[] params);
-
-    public void clearUser();
-    
-}
